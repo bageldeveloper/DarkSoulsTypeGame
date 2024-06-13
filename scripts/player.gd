51d04452558976869eafe0ddd01a3caff8693db5
Tom <tom@blowmage.com>
2024-06-13 17:34:03 -0600

    added camera controls and animations yipee

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,27 @@
 extends CharacterBody3D
 
+@onready var camera_mount = $camera_mount
+@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals = $visuals
 
-const SPEED = 5.0
+
+const SPEED = 3
 const JUMP_VELOCITY = 4.5
 
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion: 
+		rotate_y(deg_to_rad(-event.relative.x*sens_horizontal))
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y*sens_vertical))
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,12 +34,17 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if animation_player.current_animation != "walking":
+			animation_player.play("walking")
+		visuals.look_at(position + direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
